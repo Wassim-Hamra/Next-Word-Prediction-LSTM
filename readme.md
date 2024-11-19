e85c50eff655c91e3e068a4ab5ad2f9876c98c0a
Mohamed Wassim Hamra <wassimhamraa@gmail.com>
2024-11-19 11:20:35 +0100

    first push

---
+++ readme.md
@@ -0,0 +1,20 @@
+# **Next Word Prediction Using LSTM Architecture**
+
+## This project aims to develop a deep learning model for predicting the next word in a given sequence of words. The model is built using Long Short-Term Memory (LSTM) networks, which are well-suited for sequence prediction tasks and it is trained on shakespeare's hamlet dataset.
+
+## ***Steps:***
+
+### ***1- Data Collection:*** I used the text of Shakespeare's \"Hamlet\" as my dataset. This complex text provides a good challenge for the LSTM model given its length.
+
+### ***2- Data Preprocessing:*** The text data is tokenized, converted into sequences, and padded to ensure uniform input lengths. The sequences are then split into training and testing sets.
+
+### ***3- Model Building:*** The LSTM model is constructed with an embedding layer, two LSTM layers, and a dense output layer with a softmax activation function to predict the probability of the next word.
+
+  ### ***4- Model Training:*** The model is trained using the prepared sequences, with dropout implemented to prevent overfitting.
+  
+  ### ***5- Model Evaluation:*** The model is then evaluated using a set of example sentences to test its ability to predict the next word accurately.
+  
+  ### ***6- Deployment:*** Deployed using Streamlit to allow real-time predictions given an input.
+  *  ***Web App link:***
+
+
---
+++ streamlit_app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import numpy as np
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Load the LSTM model
+model = load_model('next_word_lstm.keras')
+
+# Load Tokenizer
+with open('tokenizer.pickle','rb') as handle:
+    tokenizer = pickle.load(handle)
+
+def predict_next_word(model, tokenizer, text, max_sequence_len):
+  token_list = tokenizer.texts_to_sequences([text])[0]
+  if len(token_list) >= max_sequence_len:
+    token_list = token_list[-(max_sequence_len-1):]  # Ensure the sequence length matches max_sequence_len-1\n",
+  token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+  predicted = model.predict(token_list, verbose=0)
+  predicted_word_index = np.argmax(predicted, axis=1)
+  for word, index in tokenizer.word_index.items():
+    if index == predicted_word_index:
+      return word
+  return None
+
+# Streamlit app
+st.markdown(
+    f"""
+    <style>
+    .box {{
+        padding: 1.5em 0.5em 0em 1em;
+        margin: 0em;
+        background-color:rgba(79, 227, 94, 0.3);
+        border-radius: 10px;
+        border: 0.5px solid #d3d3d3;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title('Next Word Prediction With LSTM')
+input_text = st.text_input('Enter your sequence 💬','Hello there')
+if st.button("Predict Next Word"):
+    next_word = predict_next_word(model,tokenizer,input_text,14)
+    st.markdown(f'<div class="box"><h5>{input_text}</h5><h4 style="color:red; font-weight:bold">{next_word}</h4></div>', unsafe_allow_html=True)
