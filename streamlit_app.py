72bbd3fbf0e39d73fa8255c40b30d1f35cce533c
Wassim <wassimhamraa@gmail.com>
2025-05-06 17:55:07 +0100

    tp

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 st.title('💻 Projet Tech.Multimedia : ')
-st.title('----------------')
+st.title('──── ୨୧ ────')
 st.title('✨ Prédiction du mot suivant avec RNN')
 input_text = st.text_input('Entrez votre séquence 💬','Hello there')
 if st.button("Prédire le mot suivant"):
